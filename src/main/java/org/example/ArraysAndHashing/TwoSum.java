ba562984bdb8292c7563311a1b08a0869c963744
renasustek <renasustek@gmail.com>
2025-06-18 15:40:15 +0100

    3/150 - refactored

--- src/main/java/org/example/ArraysAndHashing/TwoSum.java
+++ src/main/java/org/example/ArraysAndHashing/TwoSum.java
@@ -1,14 +1,17 @@
 package org.example.ArraysAndHashing;
 
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return null;
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for (int i = 0; i < nums.length; i++) {
+           if (map.containsKey(target - nums[i])) {
+               System.out.println(map.get(target - nums[i]));
+               return new int[]{i, map.get(target - nums[i])};
+           }
+           map.put(nums[i], i);
+       }
+       return null;
     }
 }
