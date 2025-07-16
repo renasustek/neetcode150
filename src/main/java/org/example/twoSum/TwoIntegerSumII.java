38c027cc6acba3521ba5eada1ca96e16fb555d28
renasustek <renasustek@gmail.com>
2025-07-16 19:34:47 +0100

    11/150

---
+++ src/main/java/org/example/twoSum/TwoIntegerSumII.java
@@ -0,0 +1,23 @@
+package org.example.twoSum;
+
+public class TwoIntegerSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i]+numbers[j] > target){
+                j--;
+            }
+            if (numbers[i]+numbers[j] < target){
+                i++;
+            }
+            if (numbers[i]+numbers[j] == target){
+                break;
+            }
+
+
+        }
+        return new int[]{i+1, j+1};
+    }
+
+}
---
+++ src/test/java/org/example/twoSum/TwoIntegerSumIITest.java
@@ -0,0 +1,44 @@
+package org.example.twoSum;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoIntegerSumIITest {
+
+    private final TwoIntegerSumII solution = new TwoIntegerSumII();
+
+    @Test
+    void testTwoSumIIExample1() {
+        int[] numbers = {1, 2, 3, 4};
+        int target = 3;
+        int[] expected = {1, 2};
+        int[] actual = solution.twoSum(numbers, target);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testTwoSumIIWithNegativeNumbers() {
+        int[] numbers = {-3, 0, 2, 5, 8};
+        int target = 5;
+        int[] expected = {1, 5};
+        int[] actual = solution.twoSum(numbers, target);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testTwoSumIIAtEnds() {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {1, 2};
+        int[] actual = solution.twoSum(numbers, target);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void one() {
+        int[] numbers = {-5,-3,0,2,4,6,8};
+        int target = 5;
+        int[] expected = {2, 7};
+        int[] actual = solution.twoSum(numbers, target);
+        assertArrayEquals(expected, actual);
+    }
+}
